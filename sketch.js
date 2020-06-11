cb91c86f416ff7030c957522f70b75e55ec49a14
Arnav-gamer <bhushan.palhade@gmail.com>
2020-06-11 17:06:43 +0530

    project26

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
+var box1, box2,box3,box4,box5;
 var backgroundImg,platform;
 var bird, slingShot;
 
@@ -19,22 +19,25 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
+    ground1 = new Ground(600,200,200,20);
+    box1 = new Box(600,195,30,30);
+    box2 = new Box(600,165,30,30);
+    box3 = new Box(600,145,30,30);
+    box4 = new Box(600,115,30,30);
+    box5 = new Box(635,115,30,30);
+    box6 = new Box(565,115,30,30);
+    box7 = new Box(565,115,30,30);
+    box8 = new Box(565,115,30,30);
+    box9 = new Box(635,115,30,30);
+    box10 = new Box(635,115,30,30);
+    box11 = new Box(670,115,30,30);
+    box12 = new Box(670,115,30,30);
+    box13 = new Box(530,100,30,30);
+    box14 = new Box(530,100,30,30);
+    
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+   
 
     bird = new Bird(100,100);
 
@@ -46,20 +49,24 @@ function draw(){
     background(backgroundImg);
     Engine.update(engine);
     strokeWeight(4);
+   
+    ground.display();
+    ground1.display();
     box1.display();
     box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
     box5.display();
-    log4.display();
-    log5.display();
+    box6.display();
+    box7.display();
+    box8.display(); 
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    
 
     bird.display();
     platform.display();
